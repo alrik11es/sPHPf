ebeeb0f4cca37e15c1352417e4fb4452d5b6a132
Alrik <alrik11es@gmail.com>
2011-12-13 12:23:12 +0100

    Forget to delete a Exception in mainController.php for tests.

--- controller/mainController.php
+++ controller/mainController.php
@@ -7,7 +7,6 @@ class mainController extends \Application {
     function index() {
         // This is how you set up vars to use inside your view.
         $this->data['example_var'] = \ExampleVendor\ExampleVendor::EXAMPLE_CONST;
-        throw new \Exception("Prueba");
         return new \coldstarstudios\framework\Response('web/index.twig', $this->data);
     }
 
