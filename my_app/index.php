f6538f8d4a50a002979c92330c96b70ae7e73bfa
Alrik <alrik11es@gmail.com>
2012-01-30 10:57:24 +0100

    Changes in experimental to allow the Autoloader.php to recognize the folders of the apps.

--- my_app/index.php
+++ my_app/index.php
@@ -3,7 +3,7 @@ session_start();
 use coldstarstudios\framework\Controller;
 
 require '../sPHPf/Autoloader.php';
-Autoloader::register();
+Autoloader::register(dirname(__FILE__));
 
 // Load a new instance of the application and load the selected controller with selected action.
 Application::start(new Controller('controller\mainController', 'index'));
--- sPHPf/Autoloader.php
+++ sPHPf/Autoloader.php
@@ -7,8 +7,8 @@ class Autoloader{
     }
     
     static public function autoload($class) {
-        /*// Debug purposes
-        echo dirname(__FILE__).'/widget/'.str_replace(array('\\','_',"\0"),
+        // Debug purposes
+        echo dirname(__FILE__).'/'.str_replace(array('\\','_',"\0"),
             array('/', '/', ''), $class).'.php<br>';//*/
         
         // If class not exists
